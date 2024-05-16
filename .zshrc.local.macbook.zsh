15ad8b924f8948e60532042d67adbd79ce4e5587
Gerrit Wanderer <gerrit.wanderer@on-running.com>
2024-05-16 11:57:49 +0200

    move local zshrc config file to a new system specific file

---
+++ .zshrc.local.macbook.zsh
@@ -0,0 +1,11 @@
+[[ ! -f ~/.p10k.macbook.zsh ]] || source ~/.p10k.macbook.zsh
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+DISABLE_AUTO_TITLE="true"
+function stitle() { echo -en "\e]2;$@\a" }
+
+export PATH="/opt/homebrew/opt/mysql@8.0/bin:$PATH"
+export PATH="$(yarn global bin):$PATH"
