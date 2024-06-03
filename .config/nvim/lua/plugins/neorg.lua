e287cd44aea443b51ac3a101f68954375ba06339
Gerrit Wanderer <gerrit.wanderer@on-running.com>
2024-06-03 22:29:11 +0200

    update configs and disable neorg

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -1,31 +1,31 @@
 return {
-  { "vhyrro/luarocks.nvim", priority = 1000, config = true, },
-  {
-    "nvim-neorg/neorg",
-    dependencies = { "luarocks.nvim" },
-    lazy = false,
-    version = "*",
-    config = function()
-      require('neorg').setup {
-        load = {
-            ["core.defaults"] = {},
-            ["core.concealer"] = {},
-            ["core.dirman"] = {
-                config = {
-                    workspaces = {
-                        notes = "~/Workspace/notes",
-                    },
-                    default_workspace = "notes"
-                }
-            },
-            ["core.journal"] = {
-              config = {
-                  workspace = "notes"
-              }
-          }
-        }
-      }
-
-    end,
-  }
+  -- { "vhyrro/luarocks.nvim", priority = 1000, config = true, },
+  -- {
+  --   "nvim-neorg/neorg",
+  --   dependencies = { "luarocks.nvim" },
+  --   lazy = false,
+  --   version = "*",
+  --   config = function()
+  --     require('neorg').setup {
+  --       load = {
+  --           ["core.defaults"] = {},
+  --           ["core.concealer"] = {},
+  --           ["core.dirman"] = {
+  --               config = {
+  --                   workspaces = {
+  --                       notes = "~/Workspace/notes",
+  --                   },
+  --                   default_workspace = "notes"
+  --               }
+  --           },
+  --           ["core.journal"] = {
+  --             config = {
+  --                 workspace = "notes"
+  --             }
+  --         }
+  --       }
+  --     }
+  --
+  --   end,
+  -- }
 }
