c6a21c4a080069a451c5436bf11a33c0c2ef6a0a
Gerrit Wanderer <gerrit.wanderer@on-running.com>
2024-09-06 10:09:29 +0200

    add custom colorscheme config and keymaps

--- .config/nvim-lazy/lua/config/keymaps.lua
+++ .config/nvim-lazy/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+vim.keymap.set("i", "jj", "<ESC>", { desc = "exit insert mode with jj" })
+
+-- Move multiple lines
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", {})
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", {})
