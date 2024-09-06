0d49fd6764900c3cd7161e42b908f96a2ab1f816
Gerrit Wanderer <gerrit.wanderer@on-running.com>
2024-09-06 10:08:35 +0200

    add custom configs for neo-tree

---
+++ .config/nvim-lazy/lua/plugins/extend-neo-tree.lua
@@ -0,0 +1,33 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    buffers = {
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --              -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+    },
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = { "node_modules", "git" },
+      },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+    },
+    event_handlers = {
+      {
+        event = "file_opened",
+        handler = function()
+          require("neo-tree.command").execute({ action = "focus" })
+        end,
+      },
+    },
+  },
+}
