9282d9486e8b35c7a1a1b19256f8393ea35f3071
Gerrit Wanderer <gerrit.wanderer@on-running.com>
2024-09-06 22:33:32 +0200

    restore move lines up/down by enabling alt key on mac with kitty

--- .config/nvim-lazy/lua/config/keymaps.lua
+++ .config/nvim-lazy/lua/config/keymaps.lua
@@ -3,7 +3,3 @@
 -- Add any additional keymaps here
 --
 vim.keymap.set("i", "jj", "<ESC>", { desc = "exit insert mode with jj" })
-
--- Move multiple lines
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", {})
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", {})
