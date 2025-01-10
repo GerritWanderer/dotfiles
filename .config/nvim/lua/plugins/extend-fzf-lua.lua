7290232539987370df56cc41be81459e958205a7
Gerrit Wanderer <gerrit.wanderer@on-running.com>
2025-01-10 13:55:05 +0100

    open multiple files from the fzf file search in new buffers

---
+++ .config/nvim/lua/plugins/extend-fzf-lua.lua
@@ -0,0 +1,11 @@
+local actions = require("fzf-lua").actions
+return {
+  "ibhagwan/fzf-lua",
+  opts = {
+    actions = {
+      files = {
+        ["enter"] = actions.file_edit,
+      },
+    },
+  },
+}
