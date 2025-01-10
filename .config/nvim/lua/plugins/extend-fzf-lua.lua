0e15cb0b6b90b2b8967ead16a0f3f0485c2b7934
Gerrit Wanderer <gerrit.wanderer@on-running.com>
2025-01-10 22:17:11 +0100

    enable history for file search and grep in fzf

--- .config/nvim/lua/plugins/extend-fzf-lua.lua
+++ .config/nvim/lua/plugins/extend-fzf-lua.lua
@@ -2,8 +2,19 @@ local actions = require("fzf-lua").actions
 return {
   "ibhagwan/fzf-lua",
   opts = {
+    files = {
+      fzf_opts = {
+        ["--history"] = vim.fn.stdpath("data") .. "/fzf-lua-files-history",
+      },
+    },
+    grep = {
+      fzf_opts = {
+        ["--history"] = vim.fn.stdpath("data") .. "/fzf-lua-grep-history",
+      },
+    },
     actions = {
       files = {
+        true,
         ["enter"] = actions.file_edit,
       },
     },
