79ceca92e1ec3d5f3f821c683f6bbb3cd275d4cd
Gerrit Wanderer <gerrit.wanderer@on-running.com>
2025-03-05 10:53:39 +0100

    simplity solidus startup script for tmux

--- .config/oh-my-zsh/scripts/tmux-session-solidus.sh
+++ .config/oh-my-zsh/scripts/tmux-session-solidus.sh
@@ -5,7 +5,7 @@ if ! tmux has-session -t '=solidus' 2>/dev/null; then
   tmux split-window -c ~/Workspace/solidus -t 1 -h zsh
   tmux split-window -c ~/Workspace/solidus -t 2 -v zsh
   tmux send-keys -t 1 'nvim' C-m
-  tmux send-keys -t 2 'nvm use && gulp' C-m
+  tmux send-keys -t 2 'gulp' C-m
   tmux send-keys -t 3 'rails c' C-m
   tmux resize-pane -t 1 -x '80%'
   tmux select-pane -t 1
