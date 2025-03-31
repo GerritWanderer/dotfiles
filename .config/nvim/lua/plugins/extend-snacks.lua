934fc13283bc7c43a46117ad0e223146bcd92784
Gerrit Wanderer <gerrit.wanderer@gmail.com>
2025-03-31 22:00:34 +0200

    replace neo-tree with snacks/explorer

---
+++ .config/nvim/lua/plugins/extend-snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        explorer = {
+          auto_close = true,
+          layout = {
+            present = "sidebar",
+            preview = true,
+          },
+        },
+      },
+    },
+  },
+}
