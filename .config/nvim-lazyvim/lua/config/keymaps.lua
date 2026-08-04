2cb71478ae7c7c41fe3c2f3d2fc5a99472ae856b
Gerrit Wanderer <gerrit.wanderer@on-running.com>
2026-08-04 11:22:57 +0200

    add smrtsplits navigation with herdr for lazyvim

--- .config/nvim-lazyvim/lua/config/keymaps.lua
+++ .config/nvim-lazyvim/lua/config/keymaps.lua
@@ -1,5 +1,9 @@
 -- Native split navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top split" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+-- vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+-- vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom split" })
+-- vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top split" })
+-- vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
+vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
+vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
