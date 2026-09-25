b77ebf4e7e209b3436fcd039b2fad3f8b9059285
Gerrit Wanderer <gerrit.wanderer@gmail.com>
2026-09-25 11:08:28 +0200

    Remove default marks keymaps and write a new keymap to delete marks on the current line

--- .config/nvim-omarchy/lua/config/keymaps.lua
+++ .config/nvim-omarchy/lua/config/keymaps.lua
@@ -6,16 +6,28 @@ vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
 vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
 vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
 
--- Marks & jumps (default Neovim behavior, described here so Keycade LazyVim
--- can surface them as trainable cards)
-vim.keymap.set("n", "m", "m", { desc = "Set mark" })
-vim.keymap.set("n", "`", "`", { desc = "Jump to mark (exact position)" })
-vim.keymap.set("n", "'", "'", { desc = "Jump to mark (start of line)" })
-vim.keymap.set("n", "``", "``", { desc = "Jump to last position (exact)" })
-vim.keymap.set("n", "''", "''", { desc = "Jump to last position (line)" })
-vim.keymap.set("n", "`.", "`.", { desc = "Jump to last change" })
-vim.keymap.set("n", "`^", "`^", { desc = "Jump to last insert" })
-vim.keymap.set("n", "`[", "`[", { desc = "Jump to start of last yank/change" })
-vim.keymap.set("n", "`]", "`]", { desc = "Jump to end of last yank/change" })
-vim.keymap.set("n", "<C-o>", "<C-o>", { desc = "Jump back in jumplist" })
-vim.keymap.set("n", "<C-i>", "<C-i>", { desc = "Jump forward in jumplist" })
+-- Delete all marks (a-z, A-Z) on the current line (no built-in equivalent;
+-- `m-` mirrors the vim-signature convention)
+vim.keymap.set("n", "m-", function()
+  local buf = vim.api.nvim_get_current_buf()
+  local line = vim.api.nvim_win_get_cursor(0)[1]
+  local deleted = {}
+  for c = ("a"):byte(), ("z"):byte() do
+    local name = string.char(c)
+    if vim.api.nvim_buf_get_mark(buf, name)[1] == line then
+      vim.api.nvim_buf_del_mark(buf, name)
+      table.insert(deleted, name)
+    end
+  end
+  for c = ("A"):byte(), ("Z"):byte() do
+    local name = string.char(c)
+    local m = vim.api.nvim_get_mark(name, {})
+    if m[1] == line and m[3] == buf then
+      vim.api.nvim_del_mark(name)
+      table.insert(deleted, name)
+    end
+  end
+  if #deleted > 0 then
+    vim.notify("Deleted marks: " .. table.concat(deleted, " "))
+  end
+end, { desc = "Delete marks on current line" })
